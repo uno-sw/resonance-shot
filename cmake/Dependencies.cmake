c8f7510836feac03db5214abf76dd3009f41a62d
uno-sw <uno-sw@live.jp>
2026-07-20 22:43:54 +0900

    test: Catch2 を導入

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,6 +2,8 @@ include(FetchContent)
 
 set(RESONANCE_SDL_VERSION "3.4.12")
 set(RESONANCE_SDL_COMMIT "f87239e71e42da91ca317a12eefb82cfbf3393eb")
+set(RESONANCE_CATCH2_VERSION "3.15.2")
+set(RESONANCE_CATCH2_COMMIT "191fa38c9b1596cd2576ab531d4ab4d5e8e05190")
 
 # Build SDL into the executable so development and distribution do not depend on an externally
 # installed SDL dylib.
@@ -28,6 +30,12 @@ FetchContent_Declare(
   SDL3
   GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
   GIT_TAG ${RESONANCE_SDL_COMMIT}
+  GIT_PROGRESS TRUE)
+
+FetchContent_Declare(
+  Catch2
+  GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+  GIT_TAG ${RESONANCE_CATCH2_COMMIT}
   GIT_PROGRESS TRUE)
 
 FetchContent_MakeAvailable(SDL3)
